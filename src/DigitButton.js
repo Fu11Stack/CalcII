d1d4bdd5757fa437ecb2f7e1ca5f9d0ed20cc3aa
Fu11Stack <96635777+Fu11Stack@users.noreply.github.com>
2022-11-14 16:25:20 -0800

    Add files via upload

--- src/DigitButton.js
+++ src/DigitButton.js
@@ -1,7 +1,9 @@
-import { ACTIONS } from './App'
+import { ACTIONS } from "./App"
 
-export default function DigitButton({ dispatch, digit}) {
- return <button onCLick={() => dispatch({type: ACTIONS.ADD_DIGIT, payload: {digit} })}>
+export default function DigitButton({ dispatch, digit }) {
+ return (
+ <button onCLick={() => dispatch({ type: ACTIONS.ADD_DIGIT, payload: { digit } })}>
   {digit}
  </button>
+ )
 }
